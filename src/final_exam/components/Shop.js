d5db0c8b04ab3c18c4fc6898869daa83ab423242
liushuyu6666 <liushuyu6666@users.noreply.github.com>
2020-10-07 21:20:52 -0400

    shop

--- src/final_exam/components/Shop.js
+++ src/final_exam/components/Shop.js
@@ -5,6 +5,17 @@ import LeftPanel from './leftPanel';
 class Shop extends React.Component {
     state = {
         // todo: Q1 - write your answer to init state
+        item: {"hat": 0,
+        "gloves": 0,
+        "shirt": 0,
+        "coat": 0,
+        "skirt": 0,
+        "trousers": 0,
+        "scarf": 0,
+        "sneakers": 0,
+        "beanie": 0},
+        total: 0,
+
 
 
         // write code above
@@ -12,14 +23,16 @@ class Shop extends React.Component {
 
     addItem = (item) => {
         // todo: Q2 - write your answer to add cart item
-
+        this.setState({item: {...this.state.item, [item.name]: this.state.item[item.name] + 1}},
+            ()=>console.log(this.state.item))
 
         // write code above
     };
 
     removeItem = (item) => {
         // todo: Q3 - write your answer to remove cart item
-
+        this.setState({item: {...this.state.item, [item.name]: this.state.item[item.name] === 0?
+        0: this.state.item[item.name] - 1}}, () => console.log(this.state.item));
 
         // write code above
     };
@@ -27,7 +40,9 @@ class Shop extends React.Component {
     calculateTotal() {
         let sum = 0;
         // todo: Q4 - write your answer to calculate the sum price
-
+        this.props.items.map(item => {
+            sum += item.price * this.state.item[item.name]
+        })
 
         // write code above
         return sum;
